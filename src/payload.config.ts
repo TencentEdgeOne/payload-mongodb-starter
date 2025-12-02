1de2319f54851386b6800abdaa825ca23349a44a
zoejiezhou <zoejiezhou@tencent.com>
2025-12-02 14:20:05 +0800

    init

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -79,8 +79,7 @@ export default buildConfig({
           accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
           secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
         },
-        region: process.env.S3_REGION || 'ap-guangzhou',
-        // 腾讯云 COS 端点
+        region: process.env.S3_REGION,
         endpoint: process.env.S3_ENDPOINT,
         forcePathStyle: true,
       },
